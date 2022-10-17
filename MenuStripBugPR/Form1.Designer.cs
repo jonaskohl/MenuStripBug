4e4b548d05a3211e3012b4c386b87c7cc22ce5df
Jonas Kohl <github@jonaskohl.de>
2022-10-17 22:59:31 +0200

    Added PR testing code

---
+++ MenuStripBugPR/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace MenuStripBug
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menu1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.item11ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.item12ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.item21ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.item22ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu1ToolStripMenuItem,
+            this.menu2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(662, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menu1ToolStripMenuItem
+            // 
+            this.menu1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.item11ToolStripMenuItem,
+            this.item12ToolStripMenuItem});
+            this.menu1ToolStripMenuItem.Name = "menu1ToolStripMenuItem";
+            this.menu1ToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.menu1ToolStripMenuItem.Text = "Menu 1";
+            // 
+            // item11ToolStripMenuItem
+            // 
+            this.item11ToolStripMenuItem.Name = "item11ToolStripMenuItem";
+            this.item11ToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.item11ToolStripMenuItem.Text = "Item 1.1";
+            // 
+            // item12ToolStripMenuItem
+            // 
+            this.item12ToolStripMenuItem.Name = "item12ToolStripMenuItem";
+            this.item12ToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.item12ToolStripMenuItem.Text = "Item 1.2";
+            // 
+            // menu2ToolStripMenuItem
+            // 
+            this.menu2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.item21ToolStripMenuItem,
+            this.item22ToolStripMenuItem});
+            this.menu2ToolStripMenuItem.Name = "menu2ToolStripMenuItem";
+            this.menu2ToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.menu2ToolStripMenuItem.Text = "Menu 2";
+            this.menu2ToolStripMenuItem.Visible = false;
+            this.menu2ToolStripMenuItem.DropDownClosed += new System.EventHandler(this.menu2ToolStripMenuItem_DropDownClosed);
+            // 
+            // item21ToolStripMenuItem
+            // 
+            this.item21ToolStripMenuItem.Name = "item21ToolStripMenuItem";
+            this.item21ToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.item21ToolStripMenuItem.Text = "Item 2.1";
+            // 
+            // item22ToolStripMenuItem
+            // 
+            this.item22ToolStripMenuItem.Name = "item22ToolStripMenuItem";
+            this.item22ToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.item22ToolStripMenuItem.Text = "Item 2.2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Step 1: Click   [here]  to show and open the hidden \"Menu 2\" item.\r\nStep 2: Move " +
+    "the mouse from \"Menu 2\" to \"Menu 1\".\r\nStep 3: An exception like this should be t" +
+    "hrown:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "here";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(63, 103);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(582, 266);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(23, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "How to trigger the bug:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 389);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu Strip Bug";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menu1ToolStripMenuItem;
+        private ToolStripMenuItem item11ToolStripMenuItem;
+        private ToolStripMenuItem item12ToolStripMenuItem;
+        private ToolStripMenuItem menu2ToolStripMenuItem;
+        private ToolStripMenuItem item21ToolStripMenuItem;
+        private ToolStripMenuItem item22ToolStripMenuItem;
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+    }
+}
